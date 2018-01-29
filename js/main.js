f022de826842ecd099f2ed696a2ae0750a7bce25
mmc <864604538@qq.com>
2018-01-29 22:55:55 +0800

    个人简历完善

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("portfolio1").onclick=function(){
+    document.getElementById("portfolioBar").className="state1";
+    document.getElementById("worksPic1").className="pic1_state1";
+    document.getElementById("worksPic2").className="pic2_state1";
+    document.getElementById("worksPic3").className="pic3_state1";
+
+};
+document.getElementById("portfolio2").onclick=function(){
+    document.getElementById("portfolioBar").className="state2";
+    document.getElementById("worksPic1").className="pic1_state2";
+    document.getElementById("worksPic2").className="pic2_state2";
+    document.getElementById("worksPic3").className="pic3_state2";
+
+};
+document.getElementById("portfolio3").onclick=function(){
+    document.getElementById("portfolioBar").className="state3";
+    document.getElementById("worksPic1").className="pic1_state3";
+    document.getElementById("worksPic2").className="pic2_state3";
+    document.getElementById("worksPic3").className="pic3_state3";
+
+};
